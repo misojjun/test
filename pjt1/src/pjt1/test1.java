1e6a33ca4d635b66cd258ace7f00d633d5f8bb8b
가족PC <PC@DESKTOP-UAFFI0I>
2021-10-09 15:28:29 +0900

    6

--- pjt1/src/pjt1/test1.java
+++ pjt1/src/pjt1/test1.java
@@ -5,8 +5,8 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("test1");
-		//왼쪽1-3>1>2>5
-		//오른쪽1>2 >2>5
+		//왼쪽1-3>1>2>6
+		//오른쪽1>2 >2>6
 
 	}
 
