656456fac6c0f7bba1d5267a130a04ce288903dd
KBCARD <KBCARD@DESKTOP-D4PKDPP>
2021-10-09 13:06:44 +0900

    3

--- pjt1/src/pjt1/test1.java
+++ pjt1/src/pjt1/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test1");//왼쪽1-1
+		System.out.println("test1");//왼쪽1-1-1
 	}
 
 }
